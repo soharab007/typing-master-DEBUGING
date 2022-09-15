d74cf6a288bda5af7d9b7bb7badc0a5bb0df0a57
soharab007 <soharabhossain943@gmail.com>
2022-09-15 09:40:10 +0600

    all is okay

--- history.js
+++ history.js
@@ -1,9 +1,53 @@
+// const histories = document.getElementById("histories");
+
+// function addHistory(questionText, timeTaken, errorCount) {
+//   const newRow = document.createElement("div");
+//   newRow.classList.add("card");
+
+//   newRow.innerHTML = `
+//   <h3>${questionText}</h3>
+//   <div>
+//   <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
+//   <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
+//   </div>
+//   `;
+
+//   histories.appendChild(newRow);
+
+//   let previousTests = JSON.parse(localStorage.getItem("testHistory")) || [];
+//   previousTests.push({ questionText, timeTaken, errorCount });
+//   localStorage.setItem("testHistory", JSON.stringify(previousTests));
+
+//   displayHistory();
+// }
+
+// function displayHistory() {
+//   histories.innerHTML = "";
+//   const previousTests = JSON.parse(localStorage.getItem("testHistory")) || [];
+
+//   previousTests.forEach((test) => {
+//     const newRow = document.createElement("div");
+//     newRow.classList.add("card");
+
+//     newRow.innerHTML = `
+//   <h3>${test.questionText}</h3>
+//   <p>You took: <span class="bold">${test.timeTaken}</span> seconds</p>
+//     <p>You made <span class="bold red">${test.errorCount}</span> mistakes</p>
+//   `;
+
+//     histories.appendChild(newRow);
+//   });
+// }
+
+
+
 const histories = document.getElementById("histories");
 
 function addHistory(questionText, timeTaken, errorCount) {
   const newRow = document.createElement("div");
   newRow.classList.add("card");
-
+  //const timeSecond = Math.floor(Number.parseInt(timeTaken));
+  //console.log(timeSecond);
   newRow.innerHTML = `
   <h3>${questionText}</h3>
   <div>
@@ -31,7 +75,7 @@ function displayHistory() {
 
     newRow.innerHTML = `
   <h3>${test.questionText}</h3>
-  <p>You took: <span class="bold">${test.timeTaken}</span> seconds</p>
+  <p>You took: <span class="bold">${Math.floor(test.timeTaken)}</span> seconds</p>
     <p>You made <span class="bold red">${test.errorCount}</span> mistakes</p>
   `;
 
